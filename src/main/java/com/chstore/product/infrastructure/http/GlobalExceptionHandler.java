d53260301b1ed05bc947220508ef9a1cd2f9af46
Carlos Herrera Martín <cherrera.developer@gmail.com>
2025-04-30 21:11:42 +0200

    Capture exceptions

--- src/main/java/com/chstore/product/infrastructure/http/GlobalExceptionHandler.java
+++ src/main/java/com/chstore/product/infrastructure/http/GlobalExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.chstore.product.infrastructure.http;
 
 import com.chstore.product.domain.exception.InvalidProductValueObjectException;
+import com.chstore.product.domain.exception.ProductDeleteException;
+import com.chstore.product.domain.exception.ProductNotFoundException;
+import com.chstore.product.domain.exception.ProductSaveException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -15,6 +19,21 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGenericException(Exception ex) {
+        return new ResponseEntity<>("Internal server error: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<String> handleNotFound(ProductNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ProductSaveException.class)
+    public ResponseEntity<String> handleSaveError(ProductSaveException ex) {
+        return new ResponseEntity<>("Internal server error: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ProductDeleteException.class)
+    public ResponseEntity<String> handleDeleteError(ProductDeleteException ex) {
         return new ResponseEntity<>("Internal server error: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
--- src/main/java/com/chstore/product/infrastructure/persistence/ProductRepositoryImp.java
+++ src/main/java/com/chstore/product/infrastructure/persistence/ProductRepositoryImp.java
@@ -1,5 +1,8 @@
 package com.chstore.product.infrastructure.persistence;
 
+import com.chstore.product.domain.exception.ProductDeleteException;
+import com.chstore.product.domain.exception.ProductNotFoundException;
+import com.chstore.product.domain.exception.ProductSaveException;
 import com.chstore.product.domain.model.Product;
 import com.chstore.product.domain.model.vo.ProductId;
 import com.chstore.product.domain.repository.ProductRepository;
@@ -16,19 +19,31 @@ public class ProductRepositoryImp implements ProductRepository {
     private final ProductMongoRepository mongoRepository;
     @Override
     public Product save(Product product) {
-        ProductEntity productEntity = this.mongoRepository.save(ProductMapper.INSTANCE.toEntity(product));
-        return ProductMapper.INSTANCE.toDomain(productEntity);
+        try {
+            ProductEntity productEntity = this.mongoRepository.save(ProductMapper.INSTANCE.toEntity(product));
+            return ProductMapper.INSTANCE.toDomain(productEntity);
+        } catch (RuntimeException e){
+            throw new ProductSaveException("id: " + product.getId().getValue(), e);
+        }
     }
 
     @Override
     public Optional<Product> findById(ProductId id) {
-        Optional<ProductEntity> productEntity = this.mongoRepository.findById(id.getValue());
+        try {
+            Optional<ProductEntity> productEntity = this.mongoRepository.findById(id.getValue());
 
-        return productEntity.map(ProductMapper.INSTANCE::toDomain);
+            return productEntity.map(ProductMapper.INSTANCE::toDomain);
+        } catch (RuntimeException e){
+            throw new ProductNotFoundException(id);
+        }
     }
 
     @Override
     public void deleteById(ProductId id) {
-        this.mongoRepository.deleteById(id.getValue());
+        try {
+            this.mongoRepository.deleteById(id.getValue());
+        } catch (RuntimeException e){
+            throw new ProductDeleteException(id, e);
+        }
     }
 }
